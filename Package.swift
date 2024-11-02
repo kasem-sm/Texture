4ee3c95d58babf43a30fe7d46c87ecd575cd6810
Kasem SM <kasemsm69@gmail.com>
2024-11-02 08:52:34 +0530

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -4,18 +4,24 @@ import PackageDescription
 let package = Package(
     name: "Texture",
     platforms: [
-        .iOS(.v11)
+        .iOS(.v17)
+    ],
+    targets: [
+        .target(
+            name: "AsyncDisplayKit",
+            path: "Source",
+            publicHeadersPath: ".",
+            cSettings: [
+                .headerSearchPath("../Source_Common"),
+            ],
+            cxxSettings: [
+                .headerSearchPath("../Source_Common"),
+            ]
+        )
     ],
     products: [
         .library(
             name: "AsyncDisplayKit",
             targets: ["AsyncDisplayKit"])
-    ],
-    targets: [
-        .binaryTarget(
-            name: "AsyncDisplayKit",
-            url: "https://github.com/kasem-sm/Texture/releases/download/3.2.0/AsyncDisplayKit.xcframework.zip",
-            checksum: "9a142a5cd0fd6a07a9f494deeb2803833724d66faad5687f978645585bc0ef4a"
-        )
     ]
 )
